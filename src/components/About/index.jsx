b5aeadc43467c1a558ba6d015b211cb0d7c89bca
Joaovitorcgds <joaovitorcgds@gmail.com>
2024-12-21 21:16:03 -0300

    teste layout about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,43 +1,43 @@
 import React from 'react';
-
+import image1 from "../../assets/image1.jpg"
 const About = () => {
   return (
     <section className="flex flex-col justify-between items-center py-10 px-4 bg-[#F8F8F8] w-full h-screen">
         
         <div>
             <h2 className="text-4xl md:text-7xl font-bold text-center text-primary mb-2">Nossos Serviços</h2>
-            <p className="text-center">Confira abaixo os serviços que o Studio de beleza Tatiane amorim pode promover para o bem-estar dos seus clientes.</p>
+            <p className="text-center mb-10">Confira abaixo os serviços que o Studio de beleza Tatiane amorim pode promover para o bem-estar dos seus clientes.</p>
         </div>
 
         <div className="grid grid-cols-2 gap-8">
             {/* Primeira Foto da Primeira Coluna */}
-            <div className="relative">
-            <img src="image1.jpg" alt="Imagem 1" className="w-full h-full object-cover" />
+            <div className="relative top-16">
+            <img src={image1} alt="Imagem 1" className="w-full h-full object-cover" />
             </div>
             
             {/* Primeira Foto da Segunda Coluna - Começando na metade da primeira imagem */}
             <div className="relative self-end">
-            <img src="image2.jpg" alt="Imagem 2" className="w-full h-full object-cover" />
+            <img src={image1} alt="Imagem 2" className="w-full h-full object-cover" />
             </div>
 
             {/* Segunda Foto da Primeira Coluna */}
-            <div className="relative self-start">
-            <img src="image3.jpg" alt="Imagem 3" className="w-full h-full object-cover" />
+            <div className="relative top-16 self-start">
+            <img src={image1} alt="Imagem 3" className="w-full h-full object-cover" />
             </div>
 
             {/* Segunda Foto da Segunda Coluna - Começando na metade da segunda imagem */}
             <div className="relative self-end">
-            <img src="image4.jpg" alt="Imagem 4" className="w-full h-full object-cover" />
+            <img src={image1} alt="Imagem 4" className="w-full h-full object-cover" />
             </div>
 
             {/* Terceira Foto da Primeira Coluna */}
-            <div className="relative self-start">
-            <img src="image5.jpg" alt="Imagem 5" className="w-full h-full object-cover" />
+            <div className="relative top-16 self-start">
+            <img src={image1} alt="Imagem 5" className="w-full h-full object-cover" />
             </div>
 
             {/* Terceira Foto da Segunda Coluna - Começando na metade da terceira imagem */}
             <div className="relative self-end">
-            <img src="image6.jpg" alt="Imagem 6" className="w-full h-full object-cover" />
+            <img src={image1} alt="Imagem 6" className="w-full h-full object-cover" />
             </div>
         </div>
     

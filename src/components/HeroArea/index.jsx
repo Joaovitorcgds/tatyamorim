9f1eb6c240574c2a5746f38c14967efc72dcbb9e
Joaovitorcgds <joaovitorcgds@gmail.com>
2025-01-17 12:50:35 -0300

    tentativa de adc thumbnail no heroarea

--- src/components/HeroArea/index.jsx
+++ src/components/HeroArea/index.jsx
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import videobg from "../../assets/videobcg.mp4"
 import imagebg from "../../assets/imgbcg.webp"
-// import { useEffect } from 'react';
+import imagebgMobile from "../../assets/imgBgMobile.webp"
 // import AOS from 'aos';
 // import 'aos/dist/aos.css';
 
@@ -13,11 +14,25 @@ export function HeroArea() {
   //   });
   // }, []);
 
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
   return (
     <>
-    <div className="relative w-full h-screen z-20">
+    <div className="relative w-full h-screen overflow-hidden z-20">
       {/*Overlay*/}
       <div className="inset-0 w-full h-screen object-cover absolute bg-overlay z-10"></div>
+
+      {/* Placeholder (Imagem estática) */}
+      {!isVideoLoaded && (
+        <img
+          src={imagebgMobile}
+          alt="Background"
+          className="absolute inset-0 w-full h-full object-cover z-10 bg-overlay"
+        />
+      )}
+      <video preload="auto" playsInline autoPlay loop muted onLoadedData={() => setIsVideoLoaded(true)}
+      className="w-screen h-screen object-cover absolute inset-0 bg-cover bg-center md:hidden">
+        <source src={videobg} type="video/mp4"/>
+      </video>
 
       {/* Hero Area */}
       <img src={imagebg} className="w-screen h-screen object-cover absolute inset-0 hidden md:block"
@@ -28,9 +43,6 @@ export function HeroArea() {
         <h1 className="font-playfair text-center mb-1 text-5xl md:text-7xl">Tatiane Amorim</h1>
         <span className="font-playfair text-center text-lg md:text-xl">Especialista em Mechas</span>
       </div>
-
-      {/* Tailwind Responsiveness: For Mobile */}
-      <video id="meuVideo" src={videobg} playsInline autoPlay loop muted className="w-screen h-screen object-cover absolute inset-0 bg-cover bg-center md:hidden"></video>
     </div>
     </>
   );
